ae4603c703a3f5ef00169ed180e1b8c980be5a55
JadyVella <felixouma379@gmail.com>
2019-12-14 15:29:13 +0300

    Add a function of a pizza in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
-var size = document.getElementById("large", "medium", "small").value;
-var crust = document.getElementById("crispy", "stuffed", "gluten", "cheese").value;
+function pizza(size, crust, topings) {
+    this.size = size;
+    this.crust = crust;
+    this.topings = topings;
+}
+
+var large = new pizza(["large", "medium", "small"], ["crispy", "stuffed", "gluten-free", "cheese-filled"], ["pepperoni", "mushroom", "onions", "sausage", "bacon", "extra cheese", "black olives", "green peppers"]);
+
+function amount(size, crust, topings) {
+    this.size = [large, medium, small];
+}
