3864ed46bcba91209aacddbe678deecd230a2a66
JadyVella <felixouma379@gmail.com>
2019-12-16 09:00:54 +0300

    Add a number input inside index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-function pizza(size, crust, topings) {
-    this.size = size;
-    this.crust = crust;
-    this.topings = topings;
-}
+var large = { name: "Large Pizza", price: 1000 };
+var medium = { name: "Medium Pizza", price: 700 };
+var small = { name: "Small Pizza", price: 500 };
 
-var large = new pizza(["large", "medium", "small"], ["crispy", "stuffed", "gluten-free", "cheese-filled"], ["pepperoni", "mushroom", "onions", "sausage", "bacon", "extra cheese", "black olives", "green peppers"]);
+var pizzaSize = { name: "Pizza Size", sizes: [large, medium, small] };
 
-function amount(size, crust, topings) {
-    this.size = [large, medium, small];
-}
+var crispy = { name: "Crispy Crust", price: 200 };
+var stuffed = { name: "Stuffed Crust", price: 200 };
+var gluten = { name: "Gluten-free Crust", price: 200 };
+var stuffed = { name: "Stuffed Crust", price: 200 };
+var cheese = { name: "Cheese-filled Crust", price: 200 };
+
+var pizzaCrust = { name: "Pizza Crust", crusts: [crispy, stuffed, gluten, stuffed, cheese] };
+
+var pepperoni = { name: "Pepperoni", price: 800 };
+var mushroom = { name: "Mushroom", price: 700 };
+var onions = { name: "Onions", price: 600 };
+var sausage = { name: "Sausage", price: 500 };
+var bacon = { name: "Bacon", price: 400 };
+var cheese = { name: "Extra cheese", price: 300 };
+var olives = { name: "Black olives", price: 200 };
+var peppers = { name: "Green peppers", price: 800 };
+
+var pizzaTopings = { name: "Pizza Topings", topings: [pepperoni, mushroom, onions, sausage, bacon, cheese, olives, peppers] };
+
+var Pizza = [size, crust, topings]
