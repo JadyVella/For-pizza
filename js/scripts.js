2658e1d70c51039dde4e3732f273850ae2e05987
JadyVella <felixouma379@gmail.com>
2019-12-20 09:48:40 +0300

    Add a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 // $(document).ready(function() {
-//     $("#order").submit(function(event) {
+//     $("#myForm").submit(function(event) {
 
 //         var priceOfPizza = function getSize() {
 //         var Size = document.getElementById("size").value;
 //         return parseInt(Size);
 //         };
-//         alert("function is working");
     
 //         var pizzaCrustPrice = function getCrustType() {
 //             var Crust = document.getElementById("crusts").value;
@@ -23,10 +22,10 @@
 //         };
 
 //         function UserOrder(size, crust, toppings, quantity) {
-//             this.newSize = size;
-//             this.newCrust = crust;
-//             this.newToppings = toppings;
-//             this.newQuantity = quantity;
+//             this.size = size;
+//             this.crust = crust;
+//             this.toppings = toppings;
+//             this.quantity = quantity;
 //         };
 
 //         var inputForUserOrder = new UserOrder(
@@ -35,7 +34,7 @@
 //             pizzaToppingsPrice(),
 //             pizzaQuantityPrice()
 //         );
-//         var totalPrice = (inputForUserOrder.newSize + inputForUserOrder.newCrust + inputForUserOrder.newToppings) * inputForUserOrder.newQuantity;
+//         var totalPrice = (inputForUserOrder.size + inputForUserOrder.crust + inputForUserOrder.toppings) * inputForUserOrder.quantity;
 //         alert(totalPrice);
 
 //             if (delivery === true) {
@@ -70,17 +69,33 @@
 // });
 
 
-$(document).ready(function() {
-        var prize = 0;
-        var pizzaSize = document.getElementsById("size");
+function fn(){
+        var price = 0;
+        var pizzaSize = document.getElementById("size");
         var pizzaSizePrice = pizzaSize.options[pizzaSize.selectedIndex].value;
-        var pizzaCrusts = document.getElementsById("medium");
-        var pizzaCrustsPrice = pizzaCrusts.input[pizzaCrusts.selectedIndex].value;
+        var pizzaCrusts = document.getElementById("crusts");
+        var pizzaCrustsPrice = pizzaCrusts.options[pizzaCrusts.selectedIndex].value;
         var pizzaTopings = document.getElementById("topings");
-        var pizzaTopingsPrice = pizzaTopings.options[pizzaTopings.selectIndex].value;
-        var pizzaNumber = document.getElementById("numbers");
+        var pizzaTopingsPrice = pizzaTopings.options[pizzaTopings.selectedIndex].value;
+        var pizzaNumber = document.getElementById("numbers").value;
+        var location = document.getElementById("location").value;
         price = parseInt(pizzaSizePrice) + parseInt(pizzaCrustsPrice) + parseInt(pizzaTopingsPrice);
-        totalPrize = price * parseInt(pizzaNumber);
-        document.getElementById("yourOrders").value = totalPrize;
+        totalPrice = price * parseInt(pizzaNumber);
+        alert("Your total price is" +totalPrice + "and your pizza will be delivered at " + location);
         event.preventDefault();
-});
+}
+
+// function fn1(){
+//         var price = 0;
+//         var pizzaSize = document.getElementById("size1");
+//         var pizzaSizePrice = pizzaSize.options[pizzaSize.selectedIndex].value;
+//         var pizzaCrusts = document.getElementById("crusts1");
+//         var pizzacrustsPrice = pizzaCrusts.options[pizzaCrusts.selectedIndex].value;
+//         var pizzaTopings = document.getElementById("toppings1");
+//         var pizzaTopingsPrice = pizzaTopings.options[pizzaTopings.selectedIndex].value;
+//         var pizzaNumber = document.getElementById("numbers1").value;
+//         price = parseInt(pizzaSizePrice) + parseInt(pizzacrustsPrice) + parseInt(pizzaTopingsPrice);
+//         totalPrice = price * parseInt(pizzaNumber);
+//         alert("Your total price is" + totalPrice);
+//         event.preventDefault();
+// };
